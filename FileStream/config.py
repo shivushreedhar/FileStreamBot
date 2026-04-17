f93d4ed6dde6426946f2a95b529fd6d0a7e1556f
shivushreedhar <131353533+shivushreedhar@users.noreply.github.com>
2026-04-17 23:14:56 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -12,16 +12,16 @@ class Telegram:
     DATABASE_URL = str(env.get('mongodb+srv://bsadmin:bsadmin12@cluster0.anisucu.mongodb.net/?appName=Cluster0'))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "BSHEGDE5"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
-    FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
-    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
+    FORCE_SUB_ID = env.get('-1002088921495', None)
+    FORCE_SUB = env.get('-1002088921495', False)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
     FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
     START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("-1001997350110", None))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("-1001997350110", None))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "5536032493")).split()))
