2947cc2a62f36d618906f35836f9dd6bc2c860c3
shivushreedhar <131353533+shivushreedhar@users.noreply.github.com>
2026-04-22 20:58:17 +0530

    Update render_template.py

--- FileStream/utils/render_template.py
+++ FileStream/utils/render_template.py
@@ -1,26 +1,33 @@
 import aiohttp
 import jinja2
 import urllib.parse
+import os
+
 from FileStream.config import Telegram, Server
 from FileStream.utils.database import Database
 from FileStream.utils.human_readable import humanbytes
+
 db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, "template")
+
 async def render_page(db_id):
-    file_data=await db.get_file(db_id)
+    file_data = await db.get_file(db_id)
+
     src = urllib.parse.urljoin(Server.URL, f'dl/{file_data["_id"]}')
     file_size = humanbytes(file_data['file_size'])
     file_name = file_data['file_name'].replace("_", " ")
 
     if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
-        template_file = "FileStream/template/play.html"
+        template_file = os.path.join(TEMPLATE_DIR, "play.html")
     else:
-        template_file = "FileStream/template/dl.html"
+        template_file = os.path.join(TEMPLATE_DIR, "dl.html")
         async with aiohttp.ClientSession() as s:
             async with s.get(src) as u:
                 file_size = humanbytes(int(u.headers.get('Content-Length')))
 
-    with open(template_file) as f:
+    with open(template_file, "r", encoding="utf-8") as f:
         template = jinja2.Template(f.read())
 
     return template.render(
